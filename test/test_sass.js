33d36655c42dc09d73b52139163464c1af137204
Ivan Ružević <ivan.ruzevic@infinum.com>
2025-04-17 08:58:26 +0200

    changing from spaces to tab indentation

--- test/test_sass.js
+++ test/test_sass.js
@@ -4,8 +4,8 @@ const sassTrue = require("sass-true");
 const sassFiles = ["media-breakpoints.scss", "custom-breakpoints.scss"];
 
 sassFiles.forEach((file) => {
-  const filePath = path.join(__dirname, file);
-  sassTrue.runSass({ describe, it }, filePath, {
-    loadPaths: ["node_modules"],
-  });
+	const filePath = path.join(__dirname, file);
+	sassTrue.runSass({ describe, it }, filePath, {
+		loadPaths: ["node_modules"],
+	});
 });
