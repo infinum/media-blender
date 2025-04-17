198d84d0a2b28b4f1c4694d8e463fb132d5d9267
Ivan Ružević <ivan.ruzevic@infinum.com>
2025-04-17 08:54:32 +0200

    updating node-sass to sass and updating tests

--- test/test_sass.js
+++ test/test_sass.js
@@ -1,12 +1,11 @@
-const path = require('path');
-const sassTrue = require('sass-true');
+const path = require("path");
+const sassTrue = require("sass-true");
 
-const sassFiles = [
-  'media-breakpoints.scss',
-  'custom-breakpoints.scss'
-];
+const sassFiles = ["media-breakpoints.scss", "custom-breakpoints.scss"];
 
 sassFiles.forEach((file) => {
   const filePath = path.join(__dirname, file);
-  sassTrue.runSass({file: filePath}, describe, it);
+  sassTrue.runSass({ describe, it }, filePath, {
+    loadPaths: ["node_modules"],
+  });
 });
